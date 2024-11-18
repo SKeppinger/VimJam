a7bbbe8ccd2d42952b2d2f220dcff659b6a2dbda
Steven Keppinger <skeppinger18@gmail.com>
2024-11-18 08:25:09 -0500

    enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,5 +3,3 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.die()
-		
-		
--- scripts/evil_platform.gd
+++ scripts/evil_platform.gd
@@ -2,11 +2,28 @@ extends StaticBody2D
 
 var active = true
 
+var sprite
+var area
+
+# Sprites
+var default_sprite = load("res://assets/evil_platform.png")
+var deactive_sprite = load("res://assets/evil_platform_deactivated.png")
+
+func _ready():
+	sprite = $Sprite2D
+	area = $Area2D
+
 func _process(_delta):
 	if is_in_group("paused"):
 		active = false
+		sprite.texture = deactive_sprite
+		set_collision_layer_value(1, false)
+		area.set_collision_mask_value(1, false)
 	else:
 		active = true
+		sprite.texture = default_sprite
+		set_collision_layer_value(1, true)
+		area.set_collision_mask_value(1, true)
 
 func _on_area_2d_body_entered(body):
 	if active and body.name == "Player":
