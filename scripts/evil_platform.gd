9a42b8d166d44ad71ca23728ceb44fb088994f01
Steven Keppinger <skeppinger18@gmail.com>
2024-11-18 08:28:07 -0500

    evil platform no longer physical

--- scripts/evil_platform.gd
+++ scripts/evil_platform.gd
@@ -17,12 +17,10 @@ func _process(_delta):
 	if is_in_group("paused"):
 		active = false
 		sprite.texture = deactive_sprite
-		set_collision_layer_value(1, false)
 		area.set_collision_mask_value(1, false)
 	else:
 		active = true
 		sprite.texture = default_sprite
-		set_collision_layer_value(1, true)
 		area.set_collision_mask_value(1, true)
 
 func _on_area_2d_body_entered(body):
